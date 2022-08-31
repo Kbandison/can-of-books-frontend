049b1ae6fcf2d20ee73080340d74dd194229e5f8
Kevin Bandison <kbandison@gmail.com>
2022-08-30 21:43:20 -0400

    added post to front end

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,7 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 import books from './img/books.jpg';
+import Button from 'react-bootstrap/Button';
 import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -22,6 +24,28 @@ class BestBooks extends React.Component {
       console.log('We have an error: ', error.response);
     }
   };
+
+  handlecreateBook = async(bookInfo) => {
+    console.log(bookInfo)
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_SERVER}/books`, bookInfo);
+      const newBook = response.data;
+      this.setState({
+        books: [...this.state.books, newBook],
+      })
+    } catch (error) {
+      console.log('error is book post: ', error.response)
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.createBook({
+      title: event.target.formName.value,
+      description: event.target.formDescription.value,
+      status: event.target.formStatus.value
+    })
+  }
 
   componentDidMount(){
     this.getBooks();
@@ -57,6 +81,25 @@ class BestBooks extends React.Component {
         ) : (
           <h3>No Books Found :(</h3>
         )}
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formTitle">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="name" placeholder="Enter Book Title" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="name" placeholder="Enter Book Description" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formStatus">
+            <Form.Label>Status</Form.Label>
+            <Form.Check type="checkbox" placeholder="Enter Book Status" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+
+        </Form>
       </>
     )
   }
